39a3ef5644bf0984750b046b84a77c51042680da
Víctor <victor.lozano1780@gmail.com>
2021-03-04 16:54:06 +0100

    Casi

--- Java-Game-02---Palabra-oculta-version0.2/Java-Game-02---Palabra-oculta-version0.2/Game_02_Palabra_oculta_0.2/src/main/java/appGrafica/Pantalla.java
+++ Java-Game-02---Palabra-oculta-version0.2/Java-Game-02---Palabra-oculta-version0.2/Game_02_Palabra_oculta_0.2/src/main/java/appGrafica/Pantalla.java
@@ -85,7 +85,6 @@ public class Pantalla extends JFrame {
 					btnIniciarJuego.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent e) {
 							iniciarJuego();
-
 						}
 
 					});
@@ -96,7 +95,6 @@ public class Pantalla extends JFrame {
 							JButton boton = (JButton) e.getSource();
 							letra = boton.getText();
 							boton.setEnabled(false);
-							System.out.println(letra);
 							compruebaLetra(letra);
 
 						}
@@ -262,7 +260,6 @@ public class Pantalla extends JFrame {
 
 		btnM = new JButton("M");
 		btnM.setBounds(0, 78, 39, 39);
-		btnM.setFont(new Font("Arial", Font.PLAIN, 9));
 		tecladoPanel.add(btnM);
 
 		btnN = new JButton("N");
@@ -275,7 +272,6 @@ public class Pantalla extends JFrame {
 
 		btnO = new JButton("O");
 		btnO.setBounds(110, 78, 39, 39);
-		btnO.setFont(new Font("Arial", Font.PLAIN, 9));
 		tecladoPanel.add(btnO);
 
 		btnP = new JButton("P");
@@ -284,7 +280,6 @@ public class Pantalla extends JFrame {
 
 		btnQ = new JButton("Q");
 		btnQ.setBounds(183, 78, 39, 39);
-		btnQ.setFont(new Font("Arial", Font.PLAIN, 9));
 		tecladoPanel.add(btnQ);
 
 		btnR = new JButton("R");
@@ -309,7 +304,6 @@ public class Pantalla extends JFrame {
 
 		btnW = new JButton("W");
 		btnW.setBounds(183, 116, 39, 39);
-		btnW.setFont(new Font("Arial", Font.PLAIN, 8));
 		tecladoPanel.add(btnW);
 
 		btnX = new JButton("X");
@@ -323,7 +317,35 @@ public class Pantalla extends JFrame {
 		btnZ = new JButton("Z");
 		btnZ.setBounds(134, 150, 39, 39);
 		tecladoPanel.add(btnZ);
-
+		
+		btnA.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnB.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnC.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnD.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnE.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnF.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnG.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnH.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnI.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnJ.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnK.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnL.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnM.setFont(new Font("Calibri", Font.PLAIN, 6));
+		btnN.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btn—.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnO.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnP.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnQ.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnR.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnS.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnT.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnU.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnV.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnW.setFont(new Font("Calibri", Font.PLAIN, 6));
+		btnX.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnY.setFont(new Font("Calibri", Font.PLAIN, 8));
+		btnZ.setFont(new Font("Calibri", Font.PLAIN, 8));
+		
 		pistasPanel = new JPanel();
 		pistasPanel.setBounds(10, 182, 221, 158);
 		contentPane.add(pistasPanel);
@@ -443,9 +465,6 @@ public class Pantalla extends JFrame {
 	public static void compruebaLetra(String letra) {
 		String resultado = "";
 		String palabraLabel = palabraSecreta.getText();
-		for (int i = 0; i < palabraLabel.length(); i++) {
-			System.out.println(i + " " + palabraLabel.charAt(i));
-		}
 		palabraSecreta.setText("");
 		boolean esta = false;
 		boolean acertado = true;
@@ -510,14 +529,14 @@ public class Pantalla extends JFrame {
 		pistasPanel.setVisible(true);
 		int random = (int) (Math.random() * listaDiez.getSize());
 		palabra = listaDiez.get(random);
-		System.out.println(palabra);
-		elegirImagen(1);
+		foto = 1;
+		elegirImagen(foto);
 		for (int i = 0; i < palabra.length(); i++) {
 			palabraSecreta.setText(palabraSecreta.getText() + " _");
-		}	
+		}
 		activarBotones();
 	}
-	
+
 	private static void activarBotones() {
 		btnA.setEnabled(true);
 		btnB.setEnabled(true);
